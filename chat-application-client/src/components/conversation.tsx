5d16f430c9113960c1055bed521ee15990432769
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2023-11-13 09:25:51 +0100

    Correction of some bugs that makes the production deployments failed

--- chat-application-client/src/components/conversation.tsx
+++ chat-application-client/src/components/conversation.tsx
@@ -1,7 +1,8 @@
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
 
 
-export default function Conversation() {
+export default function Conversation(props: { name: string }) {
   return (
-    <button className="bg-blue-500 text-white font-bold py-2 px-4 mb-2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700">Contact 1</button>
+    <button className="bg-blue-500 text-white font-bold py-2 px-4 mb-2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700">{props.name}</button>
   )
 }
