31d85c82632b6ea5e2e88d726b988d5322c5a96e
Johan682 <johan.loya@orange.fr>
2024-03-22 20:17:28 +0100

    version 2

--- chat-application-client/src/app/page.tsx
+++ chat-application-client/src/app/page.tsx
@@ -8,23 +8,24 @@ let conversations: string[] = [
   "Kaoutar",
   "Killian",
   "Gauthier",
-  "Sana"
+  "Sana",
+  "Johan"
 ]
 
 export default function Home() {
   return (
-    <div className='container mx-auto h-screen'>
+    <div className='container mx-auto h-screen bg-gray-200'>
       <div className='flex flex-col h-full'>
 
         { /* Top */}
-        <div className='flex justify-center items-center h-1/6 border-b border-gray-300'>
-          <h1 className='text-lg font-bold'>Votre conversation avec </h1>
+        <div className='flex justify-center items-center h-1/6 border-b border-gray-300 bg-blue-100'>
+          <h1 className='text-lg font-bold text-blue-800'>Votre conversation avec </h1>
         </div>
 
         { /* Middle */}
-        <div className='flex flex-1'>
+        <div className='flex flex-1 bg-white'>
           { /* Left side */}
-          <div className='w-2/12 border-r border-gray-300 flex flex-col'>
+          <div className='w-2/12 border-r border-gray-300 flex flex-col bg-blue-50'>
             {conversations.map((value, index) => <Conversation key={index} name={value} />)}
           </div>
 
@@ -35,12 +36,13 @@ export default function Home() {
         </div>
 
         { /* Bottom */}
-        <div className='flex justify-center items-center h-1/6 border-t border-gray-300'>
+        <div className='flex justify-center items-center h-1/6 border-t border-gray-300 bg-blue-100'>
           <div className='w-2/12'>
             <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 focus:outline-none focus:ring focus:border-blue-700 transition duration-300'>Ajouter un contact</button>
           </div>
           <div className='w-10/12'>
-            <textarea className='w-full h-full p-4 text-gray-400' placeholder='Commencez à taper ici...'></textarea>
+            <textarea className='w-full h-full p-4 text-gray-400 bg-gray-200' placeholder='Commencez à taper ici...'></textarea>
+
           </div>
         </div>
 
