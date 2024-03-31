73e1364dffd08dff19a1107149f4b1ca4c983e8f
batcho <loanbatcho@gmail.com>
2024-03-31 14:38:34 +0200

    Pages de connexions v4

--- chat-application-desktop/src/components/toastify.tsx
+++ chat-application-desktop/src/components/toastify.tsx
@@ -1,5 +1,14 @@
-import { ToastContainer, toast } from 'react-toastify'
+import { toast } from 'react-toastify'
 export type NotificationType = "info" | "success" | "warning" | "error" | "default"
-export const notify = (message: string, type: NotificationType = "default") => toast(message, {
-	type: type
-}) // Définir la fonction notify pour afficher la notification toast
+export const notify = (
+	message: string,
+	type: NotificationType = "default",
+	callback?: () => void,
+) => toast(
+	message,
+	{
+		type: type, 
+		onClose: () => {
+			if (callback) callback();
+		}
+}) 
