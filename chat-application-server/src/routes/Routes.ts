b5ab63e4fb03f3e4efac360108dcf2b87ec1b791
batcho <loanbatcho@gmail.com>
2024-04-09 16:21:08 +0200

    Modifs

---
+++ chat-application-server/src/routes/Routes.ts
@@ -0,0 +1,49 @@
+import { Request, Response, Application } from "express";
+import * as path from "path";
+import UserController from "../controllers/User";
+import ChatController from "../controllers/Chat";
+/**
+ * @class App
+ * @author Gilles Cédric
+ * @description this class is used to define all the routes of the application
+ * @exports
+ * @default
+ * @since 21/05/2022
+ */
+export default class Routes {
+  /**
+   * @property userController
+   * @description the User controller
+   * @private
+   * @readonly
+   * @type {UserController}
+   */
+  private readonly userController: UserController = new UserController();
+  private readonly ChatController: ChatController = new ChatController();
+
+  /**
+   * @method routes
+   * @description this method define all the endpoints of the application
+   * @readonly
+   * @public
+   * @returns {void}
+   */
+  public readonly routes = (app: Application): void => {
+    //Default endpoint
+    app.route("/api/").get((req: Request, res: Response) => {
+      res
+        .status(200)
+        .sendFile(
+          path.dirname(path.dirname(__dirname)) +
+          "/public/index.html".replace("/", path.sep)
+        );
+    });
+
+    app.route("/api/" + "users").get(this.userController.getAll);
+    app.route("/api/" + "users").get(this.userController.get);
+    app.route("/api/" + "users/login").post(this.userController.login);
+    app.route("/api/users/getChats").post(this.ChatController.getUserChats);
+    app.route("/api/users/getFriends").post(this.userController.getUserFriends);
+
+  };
+}
