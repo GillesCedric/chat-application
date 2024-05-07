f6db829724bcb342db952b491cf2be56f5bcb560
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2024-05-08 01:05:42 +0200

    adding of the possibility to run multiple client instances

--- chat-application-desktop/src/modules/store/Store.ts
+++ chat-application-desktop/src/modules/store/Store.ts
@@ -1,12 +1,27 @@
 import Store from 'electron-store'
+import { app } from 'electron';
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs'
 
 export default class SecureStore {
 	private secretKey: string
 	private store: Store
+	private instancePath: string
 
 	constructor(secretKey: string) {
 		this.secretKey = secretKey
-		this.store = new Store()
+		this.instancePath = path.join(app.getPath('userData'), `chat-application-${crypto.randomInt(100)}`)
+		this.createStore()
+		this.store = new Store({
+			cwd: this.instancePath
+		})
+	}
+
+	private createStore() {
+		if (!fs.existsSync(this.instancePath)) {
+			fs.mkdirSync(this.instancePath, { recursive: true });
+		}
 	}
 
 	set(key: string, value: any): void {
